ca3112cd6e3b5aa2a5c38b329e5383128fe90515
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-09 21:31:47 +0300

    options buttons moved, google place api

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "19ae8564c21dd4372e68de634a3ef75a",
-"/": "19ae8564c21dd4372e68de634a3ef75a",
-"main.dart.js": "1735661a95e9696c7e7b02787e2c4fa1",
+"index.html": "c109e7184e265053154adc643107dc1a",
+"/": "c109e7184e265053154adc643107dc1a",
+"main.dart.js": "ed18f355d79c9e8bb7644b8077b3204c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -33,7 +33,7 @@ const RESOURCES = {
 "assets/home_on.png": "939bd43d7c572ace549530d9789a08ae",
 "assets/profile_off.png": "187d83a0b39e660e710d9b7574a18983",
 "assets/add_photo.png": "9d7df1d8e1f3234735b095c900b20440",
-"assets/NOTICES": "65cb340ab790ba9600d04f800c3b2f38",
+"assets/NOTICES": "223bbd2a1fd142db7947ec60ca59a45a",
 "assets/search.png": "481fb4b9e78a969f79fa13e3445db758",
 "assets/big_marker.png": "db44ed1c81abb6d91913de04d13c0d7a",
 "assets/apple.png": "f4a94e1c1abcb71e555400c2d77fdff6",
