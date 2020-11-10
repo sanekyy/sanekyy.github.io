9d508840aca1b594778f0728648b78c46ca9a68b
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-11 02:15:32 +0300

    pick image on web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "69f06ac45943fe15fd126cf1614d6778",
-"/": "69f06ac45943fe15fd126cf1614d6778",
-"main.dart.js": "4ad00a5bc2818a5f65affd80943c3b0f",
+"index.html": "1cd8d7de081cc1218792e7d8dac8d86b",
+"/": "1cd8d7de081cc1218792e7d8dac8d86b",
+"main.dart.js": "9c47356b442f812da4f8e8c2cb05e424",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
