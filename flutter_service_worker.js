ec77d5e4405ab39533696dd419bff922201e10c5
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-13 02:33:50 +0300

    try to add data field

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "92c097cb7bde67263a87bf9d20aeaac8",
-"/": "92c097cb7bde67263a87bf9d20aeaac8",
-"main.dart.js": "0b1a9a897f7d84468b0f7e3697efbe19",
+"index.html": "f43b42ddbeb050acbe7c70f1cba1a0b3",
+"/": "f43b42ddbeb050acbe7c70f1cba1a0b3",
+"main.dart.js": "0c9c715f831e69816c69ef663746e081",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
