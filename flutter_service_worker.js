b020e0eb64ff95af10eb858f83a3740351218493
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-13 01:00:14 +0300

    test vk bridge event logs on mobile

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "fc037159affa9c1e44df63af958639ad",
-"/": "fc037159affa9c1e44df63af958639ad",
-"main.dart.js": "e66d8075987ebbebce4cd9bd3f70e5c6",
+"index.html": "33b7261e63bbc8dea7ea1fafffb1fb0f",
+"/": "33b7261e63bbc8dea7ea1fafffb1fb0f",
+"main.dart.js": "c1a6720df5e246561ae4856d572ba176",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
