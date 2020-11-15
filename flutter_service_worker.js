af629c1af2eea4753891c70250e9e5e6f99a299b
Iurii Dorofeev <otopba@inbox.ru>
2020-11-15 19:26:17 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "fdc4bb6aa8fc9116c3ad8863b68ede46",
-"/": "fdc4bb6aa8fc9116c3ad8863b68ede46",
-"main.dart.js": "e4f05619f78bb05c4f7eddf54be2bcf7",
+"index.html": "6ead8bc03460b30bf3ec4ac9b3b18e50",
+"/": "6ead8bc03460b30bf3ec4ac9b3b18e50",
+"main.dart.js": "c1c428f7135bb3fcac342cbf4f7db945",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
