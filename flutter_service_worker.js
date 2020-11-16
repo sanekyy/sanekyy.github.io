f3a060912e399d6acd5ab90b4b2bc215d60c5eda
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-16 16:47:30 +0300

    share logs via VK bridge copy text, test vk story

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "4a616878ea43fcbe1979e736ff07e4c9",
-"/": "4a616878ea43fcbe1979e736ff07e4c9",
-"main.dart.js": "b3fad176097cf6f50c9f2e72570c788a",
+"index.html": "608ece3b8bdf67b5cbb679bee1258261",
+"/": "608ece3b8bdf67b5cbb679bee1258261",
+"main.dart.js": "def9ef156ce73d7df2d3767e76ed8bd6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
