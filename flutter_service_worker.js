d8e97709a668528011d8cf42d9a012de6e93b105
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2020-11-16 12:43:29 +0300

    fix copy text

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "22071543f40d4cbb8fd49b0152d6edce",
-"index.html": "9ee86e8c14828bc9439281811f3a1ab1",
-"/": "9ee86e8c14828bc9439281811f3a1ab1",
-"main.dart.js": "15bccdf3b73936a2b79808742dad6907",
+"index.html": "4a616878ea43fcbe1979e736ff07e4c9",
+"/": "4a616878ea43fcbe1979e736ff07e4c9",
+"main.dart.js": "b3fad176097cf6f50c9f2e72570c788a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
